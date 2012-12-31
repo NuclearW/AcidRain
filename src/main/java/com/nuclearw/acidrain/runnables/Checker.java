99f45e1e4e90515cd7a51ca8dcc1806e79da2029
Kyle Clemens <jkc.clemens@gmail.com>
2012-12-30 22:43:29 -0500

    Updated: Use early continues instead of nested ifs

--- src/main/java/com/nuclearw/acidrain/runnables/Checker.java
+++ src/main/java/com/nuclearw/acidrain/runnables/Checker.java
@@ -1,10 +1,9 @@
 package com.nuclearw.acidrain.runnables;
 
-import org.bukkit.block.Biome;
-import org.bukkit.entity.Player;
-
 import com.nuclearw.acidrain.AcidRain;
 import com.nuclearw.acidrain.Config;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Player;
 
 public class Checker implements Runnable {
 	private AcidRain plugin;
@@ -17,39 +16,35 @@ public class Checker implements Runnable {
 	public void run() {
 		Player[] players = plugin.getServer().getOnlinePlayers();
 
-		for(Player player : players) {
-			if(player.getWorld().hasStorm()) {
-				if(!player.hasPermission("acidrain.immune")) {
-					int xLocation = player.getLocation().getBlockX();
-					int zLocation = player.getLocation().getBlockX();
+		for (Player player : players) {
+			if (!player.getWorld().hasStorm()) continue;
+			if (player.hasPermission("acidrain.immune")) continue;
+			int xLocation = player.getLocation().getBlockX();
+			int zLocation = player.getLocation().getBlockX();
 
-					Biome biomeLocation = player.getWorld().getBiome(xLocation, zLocation);
+			Biome biomeLocation = player.getWorld().getBiome(xLocation, zLocation);
 
-					switch(biomeLocation) {
-						case DESERT:
-						case DESERT_HILLS:
-						case FROZEN_OCEAN:
-						case FROZEN_RIVER:
-						case HELL:
-						case ICE_MOUNTAINS:
-						case ICE_PLAINS:
-						case SKY:
-						case TAIGA:
-						case TAIGA_HILLS:
-							continue;
-					}
-
-					int yLocation = player.getLocation().getBlockY();
-
-					int highestY = player.getWorld().getHighestBlockYAt(xLocation, zLocation);
-
-					if(yLocation > highestY) {
-						if(player.getHealth() > Config.damageCutoffLevel) {
-							player.damage(Config.damagePerInterval);
-						}
-					}
-				}
+			switch (biomeLocation) {
+				case DESERT:
+				case DESERT_HILLS:
+				case FROZEN_OCEAN:
+				case FROZEN_RIVER:
+				case HELL:
+				case ICE_MOUNTAINS:
+				case ICE_PLAINS:
+				case SKY:
+				case TAIGA:
+				case TAIGA_HILLS:
+					continue;
 			}
+
+			int yLocation = player.getLocation().getBlockY();
+
+			int highestY = player.getWorld().getHighestBlockYAt(xLocation, zLocation);
+
+			if (yLocation <= highestY) continue;
+			if (player.getHealth() <= Config.damageCutoffLevel) continue;
+			player.damage(Config.damagePerInterval);
 		}
-    }
+	}
 }
