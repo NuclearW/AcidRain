f090088af3731603e9af88db98150a5e92fa0479
SonarBeserk <sonarbeserk@github>
2012-12-31 18:03:08 -0500

    Added: Right Event, ChatColor Support

--- src/main/java/com/nuclearw/acidrain/listeners/WeatherListener.java
+++ src/main/java/com/nuclearw/acidrain/listeners/WeatherListener.java
@@ -1,29 +1,36 @@
 package com.nuclearw.acidrain.listeners;
 
+import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.weather.WeatherEvent;
-
+import org.bukkit.event.weather.WeatherChangeEvent;
 import com.nuclearw.acidrain.AcidRain;
 import com.nuclearw.acidrain.Config;
 
-public class WeatherListener implements Listener
-{
+public class WeatherListener implements Listener {
 	AcidRain plugin;
 	
-	public WeatherListener(AcidRain plugin)
-	{
+	public WeatherListener(AcidRain plugin) {
 		this.plugin = plugin;
 	}
 	
 	@EventHandler(ignoreCancelled = true)
-	public void weatherevent(WeatherEvent event)
-	{
-		if(event.getWorld().isThundering())
-		{
-			if(Config.rainwarning)
-			{
-				plugin.getServer().broadcastMessage(Config.rainwarningmsg);
+	public void weatherevent(WeatherChangeEvent event) {
+		if(event.toWeatherState() == true) {
+			if(Config.rainwarning) {
+				World world = event.getWorld();
+				
+				List<Player> players = world.getPlayers();
+				
+				for (Player player : players) {
+					String formatedmsg = Config.rainwarningmsg;
+					formatedmsg = ChatColor.translateAlternateColorCodes('&', formatedmsg);
+					
+					player.sendMessage(formatedmsg);
+				}
 			}
 		}
 	}
