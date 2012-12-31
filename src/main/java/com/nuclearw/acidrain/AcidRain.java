88521466a17dc89e15fa40c127cd2ab0759950a7
SonarBeserk <sonarbeserk@github>
2012-12-30 21:45:35 -0500

    add config.yml

--- src/main/java/com/nuclearw/acidrain/AcidRain.java
+++ src/main/java/com/nuclearw/acidrain/AcidRain.java
@@ -3,6 +3,7 @@ package com.nuclearw.acidrain;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AcidRain extends JavaPlugin {
+	
 	@Override
 	public void onEnable() {
 		getLogger().info("Finished Loading " + getDescription().getFullName());
